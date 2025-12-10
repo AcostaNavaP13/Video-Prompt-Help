ffdcc941626f0a5ae2c9a386405a6560c41eca53
Adhal Andres Cabrera Acosta <andres_acosta13@hotmail.com>
2025-12-10 18:45:46 +0000

    fix icons for PWA

---
+++ twa-avd/app/src/main/java/com/acostanavap13/avd/DelegationService.java
@@ -0,0 +1,14 @@
+package com.acostanavap13.avd;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
